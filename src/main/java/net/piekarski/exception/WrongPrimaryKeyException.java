db01e689f4fc612d24b82b156ce230e7eeb6d38f
Michal Piekarski <michal.piekarski@gmail.com>
2014-03-01 18:49:36 +0100

    add message to WrongPrimaryKeyException

--- src/main/java/net/piekarski/exception/WrongPrimaryKeyException.java
+++ src/main/java/net/piekarski/exception/WrongPrimaryKeyException.java
@@ -1,4 +1,10 @@
 package net.piekarski.exception;
 
 public class WrongPrimaryKeyException extends TableImporterException {
+    public static final String FILE_FORMAT_NOT_SUPPORTED = "column name list doesn't include primary key";
+
+    @Override
+    public String getMessage() {
+        return FILE_FORMAT_NOT_SUPPORTED;
+    }
 }
